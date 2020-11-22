6077205db9caa2f023fee6047ca625647f563217
lam <dinhtruonglam001@gmail.com>
2020-11-22 13:35:10 +0300

    class

---
+++ src/hocclass.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class person{
+    int age;
+    String name;
+}
+public class hocclass {
+    public static void main(String [] args){
+        person one = new person();
+        Scanner cin = new Scanner(System.in);
+        // day la khai bao 1 cai giong nhu bien, no khong co tac dung nhap hay gi het, chi la
+        /// khai bao
+        System.out.println("Ten");
+        one.name = cin.next();
+        // day la lenh nhap, nhu kieu cin trong c++
+        System.out.println("tuoi");
+        one.age = cin.nextInt();
+        // day cung vay
+        System.out.printf("Ten thang nay la: %s va tuoi cua no la: %d", one.name, one.age);
+    }
+}
