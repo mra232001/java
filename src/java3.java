3233778d20022b61ba297e2c4e1b37b96fe394c6
lam <dinhtruonglam001@gmail.com>
2020-11-22 11:28:47 +0300

    5

--- src/java3.java
+++ src/java3.java
@@ -1,6 +1,7 @@
 public class java3{
     public static void main(String [] args){
         String s = "lam";
-        System.out.println(s);
+        double x = 0.5;
+        System.out.println(s + x);
     }
 }
---
+++ src/java4.java
@@ -0,0 +1,9 @@
+public class java4 {
+    public static void main(String [] args){
+        System.out.println(_min(4, 5));
+    }
+    public static int _min(int x, int y){
+        if(x < y) return x;
+        else return y;
+    }
+}
