84123dd89782552d3554e075164ad5eafa39b625
lam <dinhtruonglam001@gmail.com>
2020-11-22 11:43:18 +0300

    6

--- src/java4.java
+++ src/java4.java
@@ -1,6 +1,9 @@
 public class java4 {
     public static void main(String [] args){
         System.out.println(_min(4, 5));
+        for(int i = 1; i <= 5; i++){
+            System.out.printf("Value: %d %s\n", i, "lamdeptrai");
+        }
     }
     public static int _min(int x, int y){
         if(x < y) return x;
